3ce09aadcbdc6bc15b8a4daaefe5abdc662719ac
Oliver Raduner <oliver.raduner@namics.com>
2013-07-04 10:11:40 +0200

    Some fixes
    Development version with some fixes, not complete yet.

--- VanillaFancybox/class.vanillalightbox.plugin.php
+++ VanillaFancybox/class.vanillalightbox.plugin.php
@@ -6,7 +6,7 @@
 $PluginInfo['VanillaFancybox'] = array(
 	'Name'			=> 'jQuery Fancybox',
 	'Description'	=> 'Adds the jQuery Fancybox plugin to display images in a nice way.',
-	'Version'		=> '1.1',
+	'Version'		=> '1.2',
 	'Author'		=> 'Oliver Raduner',
 	'AuthorEmail'	=> 'vanilla@raduner.ch',
 	'AuthorUrl'		=> 'http://raduner.ch/',
@@ -27,8 +27,8 @@ $PluginInfo['VanillaFancybox'] = array(
  *
  * Creates an on-site "popup" for diaplying images, using the jQuery Fancybox library
  *
- * @version 1.1
- * @date 06-NOV-2011
+ * @version 1.2
+ * @date 04-OCT-2011
  * @author Oliver Raduner <vanilla@raduner.ch>
  * 
  * @link http://fancybox.net/ jQuery Fancybox Plugin
@@ -41,7 +41,7 @@ class VanillaFancyboxPlugin extends Gdn_Plugin
 	/**
 	 * Hack the Base Render in order to achieve our goal
 	 * 
-	 * @version 1.1
+	 * @version 1.2
 	 * @since 1.0
 	 */
 	public function Base_Render_Before($Sender)
@@ -56,6 +56,40 @@ class VanillaFancyboxPlugin extends Gdn_Plugin
 		$Sender->AddCSSFile($this->GetResource('style/jquery.fancybox.css', FALSE, FALSE));
 		
 		// The jQuery configs for the Fancybox plugin
+		/* SANDBOX
+					$.get("test.php", function(data){
+						alert("Data Loaded: " + data);
+					});
+		---
+				if ($(this).parent().is("a")){
+					var src = $(this).parent("a").attr("href");
+				}
+		---		
+					if (IsValidImageUrl($(this).parent().attr("href"), ImageCheckCallback) == true){
+						$(this).wrap("<a class=\"fancybox\" href=\"" + $(this).parent("a").attr("href") + "\" />");
+					} else {
+						return false;
+					}
+		---
+		function ImageCheckCallback(url, answer){
+				return answer;
+			}
+			function IsValidImageUrl(url, callback){
+			    $("<img>", {
+			        src:	url,
+			        error:	function() { callback(url, false); },
+			        load:	function() { callback(url, true); }
+			    });
+			}
+		---
+		$("<img />")
+			.bind("error", function(event) { alert("error 1"); })
+			.bind("load", function(event) { alert("load 1"); })
+			.attr("src", "http://l.yimg.com/cv/mntl/tra/11q2/img_48880236.jpg");
+		---
+		http://stackoverflow.com/questions/4669111/is-this-a-valid-test-to-check-if-a-url-refers-to-an-image-in-js-jquery
+		http://stackoverflow.com/questions/6681741/check-for-file-existence-on-the-web-using-url-passed
+		*/
 		$FancyboxJQuerySource = '
 		<script type="text/javascript">
 		jQuery(document).ready(function(){
@@ -67,7 +101,7 @@ class VanillaFancyboxPlugin extends Gdn_Plugin
 					.attr("src", url);
 			}
 			
-			$("img, a > img").not("a.Title img, a.Photo img, a.ProfileLink img, .emoticon, div.BannerWrapper img").each(function(){
+			$("img, a > img").not("a.Title img, a.Photo img, a.ProfileLink img, .emoticon").each(function(){
 				if ($(this).parent().is("a")){
 					if (checkImageUrl($(this).parent().attr("href")) == true){
 						$(this).wrap("<a class=\"fancybox\" href=\"" + $(this).parent("a").attr("href") + "\" />");
